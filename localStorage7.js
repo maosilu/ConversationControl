0f6106c89718e12eb4ae190fa50f654a83a4a247
maosilu <1540688711@qq.com>
2017-12-25 10:11:31 +0800

    扩展localStorage，设置过期时间

--- localStorage7.js
+++ localStorage7.js
@@ -2,7 +2,7 @@
 扩展localStorage
 localStorage能够存储文本数据也能存储字符串，遇到对象的要先将它序列化为字符串之后才能存储，不是对象类型的不需要序列化
 */
-var custom_localStorage={
+/*var custom_localStorage={
 	set: function(key, value){
 		var item = {
 			data:value
@@ -16,4 +16,25 @@ var custom_localStorage={
 		val = JSON.parse(val);
 		return val;
 	}
-};
+};*/
+var custom_localStorage = {
+	//添加缓存时间：
+	set: function(key, val, days){
+		var item = {
+			data:value,
+			endTime:new Date().getTime().+days*24*3600*1000
+		};
+		localStorage.setItem(key, JSON.stringify(item));
+	},
+
+	get: function(key){
+		var val = localStorage.getItem(key);
+		if(!val) return null;
+		val = JSON.parse(val);
+		if(new Date().getTime() > val.endTime){
+			localStorage.removeItem(key);
+		}
+		return val.data;
+	}
+
+}
